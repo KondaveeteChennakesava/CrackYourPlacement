67b03c3a95404d56cdd62fb97f7fb3c06f65b1d4
Chennakesava K <96309319+KondaveeteChennakesava@users.noreply.github.com>
2025-03-01 20:40:47 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (62.06%) - LeetSync

---
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+private:
+    void moveZerosToEnd(vector<int>& nums){
+        int idx = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] != 0){
+                swap(nums[i], nums[idx]);
+                idx++;
+            }
+        }
+    }
+public:
+    vector<int> applyOperations(vector<int>& nums) {
+        for(int i=0; i<nums.size()-1; i++){
+            if(nums[i] == nums[i+1]){
+                nums[i] *= 2;
+                nums[i+1] = 0;
+            }
+        }
+        moveZerosToEnd(nums);
+        return nums;
+    }
+};
