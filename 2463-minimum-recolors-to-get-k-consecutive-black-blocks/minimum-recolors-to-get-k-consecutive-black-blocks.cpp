83d6d1de2a3db24ccb3d1dc0f3567b93b4b623ac
Chennakesava K <96309319+KondaveeteChennakesava@users.noreply.github.com>
2025-03-08 16:37:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (31.01%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.cpp
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
     int minimumRecolors(string blocks, int k) {
-        if(blocks.size() == k){
-            int count = 0;
-            for(int i=0; i<blocks.size(); i++){
-                if(blocks[i] == 'W')count++;
-            }
-            return count;
+        int ans = INT_MAX;
+        int cnt = 0;
+        for(int i=0; i<blocks.size(); i++){
+            if(blocks[i] == 'B')cnt++;
+            if(i-k>=0 && blocks[i-k]=='B')cnt--;
+            ans = min(ans, k-cnt);
         }
-        int Min = INT_MAX;
-        for(int i=0; i<blocks.size()-k+1; i++){
-            int count = 0;
-            for(int j=i; j<k+i; j++){
-                if(blocks[j] == 'W')count++;
-                // cout << blocks[j] << " ";
-            }
-            // cout << endl;
-            Min = (Min > count ? count : Min);
-        }
-        return Min;
+        return ans;
     }
 };
