b2527329f5122119738f1ea815ca86a23cf3d182
Chennakesava K <96309319+KondaveeteChennakesava@users.noreply.github.com>
2025-03-14 15:15:20 +0530

    Time: 23 ms (75.43%) | Memory: 88.2 MB (41.19%) - LeetSync

--- 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.cpp
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.cpp
@@ -1,29 +1,27 @@
 class Solution {
     #define ll long long
-
-    bool canAssign(ll& mid,vector<int>& candies,ll& k){
-        ll count=0;
-        for(ll ele: candies){
-            if(ele >= mid)
-                count += ele/mid;
+    bool canAssign(ll &mid, vector<int>& candies, ll &k){
+        ll count = 0;
+        for(ll it: candies){
+            if(it >= mid) count += it/mid;
         }
-        return count>=k;
+        return count >= k;
     }
 public:
     int maximumCandies(vector<int>& candies, long long k) {
         ll low = 1;
-        ll high = *max_element(candies.begin(),candies.end());
+        ll high = *max_element(candies.begin(), candies.end());
         ll mid;
-        ll max_candies=0;
-        while(low<=high){
-            mid = low+(high-low)/2;
-            if(canAssign(mid,candies,k)){
-                max_candies = mid;
-                low = mid+1;
+        ll ans = 0;
+        while(low <= high){
+            mid = low + (high - low) / 2;
+            if(canAssign(mid, candies, k)){
+                ans = mid;
+                low = mid + 1;
             }else{
-                high = mid-1;
+                high = mid - 1;
             }
         }
-        return max_candies;
+        return ans;
     }
 };
