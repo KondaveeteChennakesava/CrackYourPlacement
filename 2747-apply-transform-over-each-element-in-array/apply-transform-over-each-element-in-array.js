0176ab8eaddbbc646e5c5845dd5f1f2b50415cce
Chennakesava K <96309319+KondaveeteChennakesava@users.noreply.github.com>
2025-03-14 18:41:42 +0530

    Time: 39 ms (88.49%) | Memory: 53.7 MB (12.66%) - LeetSync

---
+++ 2747-apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    let ans = []
+    for(let i=0; i<arr.length; i++){
+        ans[i] = fn(arr[i], i);
+    }
+    // console.log(arr);
+    return ans;
+};
