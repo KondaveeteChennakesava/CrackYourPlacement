751b3e5f852a3bcae4db17c4532c4f0b8a5a03a7
Chennakesava K <96309319+KondaveeteChennakesava@users.noreply.github.com>
2025-03-15 07:45:21 +0530

    Time: 27 ms (48.05%) | Memory: 60.8 MB (67.69%) - LeetSync

---
+++ 2690-house-robber-iv/house-robber-iv.cpp
@@ -0,0 +1,33 @@
+class Solution {
+    bool canPlace(int &mid, vector<int> &nums, int &k){
+        int idx = 0;
+        int count = 0;
+        while(idx < nums.size()){
+            if(nums[idx] <= mid){
+                count++;
+                idx += 2;
+            }else{
+                idx += 1;
+            }
+            if(count >= k)return true;
+        }
+        return false;
+    }
+public:
+    int minCapability(vector<int>& nums, int k) {
+        int low = 1;
+        int high = *max_element(nums.begin(), nums.end());
+        int ans = 0;
+        int mid;
+        while(low <= high){
+            mid = low + (high - low) / 2;
+            if(canPlace(mid, nums, k)){
+                ans = mid;
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
